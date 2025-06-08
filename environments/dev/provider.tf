6aeecbd1f594c198de8f3b5a8014c7c48896a094
Shubham Agarwal <shubham.27052@gmail.com>
2025-06-08 21:35:24 +0530

    #1 Added the dev web app and ASP

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "azurerm" {
-      storage_account_name = "noteapprmtbackend01"
-      container_name = "remote-backend"
-      key = "global.backendsa.terraform.tfstate"
-      resource_group_name = "RGglobal"
-    }
+  backend "azurerm" {
+    storage_account_name = "noteapprmtbackend01"
+    container_name       = "remote-backend"
+    key                  = "dev.terraform.tfstate"
+    resource_group_name  = "RGglobal"
+  }
 }
 
 provider "azurerm" {
