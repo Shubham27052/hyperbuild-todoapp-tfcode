b5ddb594822c7d4aa8ec0fa195ed591a3ac0de80
Shubham Agarwal <shubham.27052@gmail.com>
2025-06-21 17:40:11 +0530

    Added role assignment for dev web app

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -37,33 +37,33 @@ resource "azurerm_linux_web_app" "webapp_dev" {
   service_plan_id     = azurerm_service_plan.asp_dev.id
 
   site_config {
-    always_on = false
+    always_on                               = false
     container_registry_use_managed_identity = true
-  
+
     application_stack {
-      docker_image_name = "noteapp/dev:latest"
+      docker_image_name   = "noteapp/dev:latest"
       docker_registry_url = "https://acrnoteapp.azurecr.io"
     }
   }
 
   identity {
-   type = "SystemAssigned"
+    type = "SystemAssigned"
   }
 
 }
 
 
 resource "azurerm_role_assignment" "roleassign_devwebapp_contributor" {
-  scope = data.terraform_remote_state.global_acr.outputs.acr_global_id
+  scope                = data.terraform_remote_state.global_acr.outputs.acr_global_id
   role_definition_name = "Contributor"
-  principal_id = azurerm_linux_web_app.webapp_dev.identity.principal_id
-  
+  principal_id         = azurerm_linux_web_app.webapp_dev.identity[0].principal_id
+
 }
 
 
 resource "azurerm_role_assignment" "roleassign_devwebapp_acrpull" {
-  scope = data.terraform_remote_state.global_acr.outputs.acr_global_id
+  scope                = data.terraform_remote_state.global_acr.outputs.acr_global_id
   role_definition_name = "AcrPull"
-  principal_id = azurerm_linux_web_app.webapp_dev.identity.principal_id
-  
+  principal_id         = azurerm_linux_web_app.webapp_dev.identity[0].principal_id
+
 }
