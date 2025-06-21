4da45623b06be7d09d7769451f212bd2fbe916eb
Shubham Agarwal <shubham.27052@gmail.com>
2025-06-21 15:57:57 +0530

    update prod

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_service_plan" "asp_prod" {
 
 
 resource "azurerm_linux_web_app" "webapp_prod" {
-  name                = "NoteAppprod"
+  name                = "NoteAppProd"
   resource_group_name = azurerm_resource_group.rg_prod.name
   location            = azurerm_resource_group.rg_prod.location
   service_plan_id     = azurerm_service_plan.asp_prod.id
